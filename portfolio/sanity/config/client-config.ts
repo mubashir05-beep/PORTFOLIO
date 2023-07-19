154582e52768728edd65735a2d673cc4b0e6a7c5
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-07-19 14:15:10 +0500

    Update

---
+++ portfolio/sanity/config/client-config.ts
@@ -0,0 +1,7 @@
+const config = {
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    apiVersion: "2023-07-19",
+  }
+  
+  export default config;
