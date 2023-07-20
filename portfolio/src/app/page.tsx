4d185482cfcb827edaae938fe4873b0140668a0a
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-07-20 15:51:18 +0500

    update

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from 'next/image'
 
-export default function Home() {
+import Image from 'next/image';
+import { getProjects,  } from '../../sanity/sanity-utils';
+import { use, useEffect, useState } from 'react';
+
+// Assuming the Project type is defined in "../../sanity/sanity-utils"
+
+
+export default async function  Home() {
+const projects=await getProjects();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-   Hello
+     {
+  projects && projects.map((project:any) => (
+    <div key={project._id}>
+      {project.project_name}
+    </div>
+  ))
+}
+
     </main>
-  )
+  );
 }
