d4c337cc7bf5f7d9358e4b25cf8e7e12a413982c
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-07-20 09:40:28 -0700

    update

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,17 +1,43 @@
-import Image from "next/image";
 import { getProjects } from "../../sanity/sanity-utils";
-import { use, useEffect, useState } from "react";
-import Hom from "@/components/Hom";
-import AnotherComponent from "@/components/AnotherComponent";
-
+import Link from "next/link";
+import './globals.css'
 // Assuming the Project type is defined in "../../sanity/sanity-utils"
 
-export default async function Home() {
-  const projects = await getProjects();
+export default function Home({}) {
+  interface Ilinks {
+    id: number;
+    name: string;
+    url: string ;
+  }
+
+  const linkData: Ilinks[] = [
+    { id: 1, name: "Projects", url: "/projects" },
+    { id: 2, name: "About", url: "/about" },
+    { id: 3, name: "Contact", url: "/contact" },
+  ];
+
   return (
-    <div>
-      <Hom/>
-      <AnotherComponent/>
-    </div>
+    <>
+      <div className="flex justify-between ">
+        <div>Mubsah1r</div>
+        <ul>
+          {linkData.map((data) => (
+            <li key={data.id}>
+              <Link href={data.url}>{data.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
+}
+// https://www.ahsankhan.me/
+// Fetch projects here and pass them as props to Home component
+export async function getStaticProps() {
+  const projects = await getProjects();
+  return {
+    props: {
+      projects,
+    },
+  };
 }
