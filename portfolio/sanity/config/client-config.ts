29d0d707400c35d8eb648c81e99443d8e7281f1e
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-07-20 15:53:58 +0500

    update

--- portfolio/sanity/config/client-config.ts
+++ portfolio/sanity/config/client-config.ts
@@ -1,15 +1,14 @@
-
-import sanityClient from '@sanity/client';
-import imageUrlBuilder from '@sanity/image-url';
+import sanityClient from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-    apiVersion: "2023-07-19",
-  useCdn:true,
-  token: process.env.NEXT_PUBLIC_SANITY_TOKEN
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: "2023-07-19",
+  useCdn: true,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
 
-export const urlFor = (source:any) => builder.image(source);
+export const urlFor = (source: any) => builder.image(source);
--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,23 +1,17 @@
-
-import Image from 'next/image';
-import { getProjects,  } from '../../sanity/sanity-utils';
-import { use, useEffect, useState } from 'react';
+import Image from "next/image";
+import { getProjects } from "../../sanity/sanity-utils";
+import { use, useEffect, useState } from "react";
 
 // Assuming the Project type is defined in "../../sanity/sanity-utils"
 
-
-export default async function  Home() {
-const projects=await getProjects();
+export default async function Home() {
+  const projects = await getProjects();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     {
-  projects && projects.map((project:any) => (
-    <div key={project._id}>
-      {project.project_name}
-    </div>
-  ))
-}
-
+      {projects &&
+        projects.map((project: any) => (
+          <div key={project._id}>{project.project_name}</div>
+        ))}
     </main>
   );
 }
