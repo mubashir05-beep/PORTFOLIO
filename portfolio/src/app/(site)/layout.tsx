e53d5a4fa0a95453e3e7073f59028fd730b27723
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-07-23 19:58:53 +0500

    Updated

--- portfolio/src/app/(site)/layout.tsx
+++ portfolio/src/app/(site)/layout.tsx
@@ -8,32 +8,29 @@ import ThemeToggle from "@/components/ThemeToggle";
 
 export const metadata = {
   title: "Mubashir | Portfolio",
-
   description: "Portfolio",
 };
+
 const inter = Poppins({
   subsets: ["latin"],
   weight: ["400", "100", "200", "300", "500", "600", "700", "800", "900"],
 });
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body
-        className={`px-[3rem] max-[500px]:px-[2rem] flex items-center justify-center h-screen ${inter.className}`}
+        className={`px-[3rem] max-[500px]:px-[2rem] flex flex-col items-center justify-center min-h-screen ${inter.className}`}
       >
         <StateContextProvider>
           <Providers>
-            <div className="flex flex-col items-center h-screen w-full ">
+            <div className="flex flex-col items-center w-full">
               <div className="flex justify-end w-full items-center pt-[0.5rem]">
                 <ThemeToggle />
               </div>
-              <div className="flex flex-col items-center w-full h-full">{children}</div>
+              <div className="flex flex-col items-center w-full flex-grow">{children}</div>
+              <Footer />
             </div>
-           
           </Providers>
         </StateContextProvider>
       </body>
