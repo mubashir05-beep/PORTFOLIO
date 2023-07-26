e20169db8d85bc388298bd896f46551b910f198b
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-07-26 22:32:39 +0500

    update

--- portfolio/src/app/(site)/contact/page.tsx
+++ portfolio/src/app/(site)/contact/page.tsx
@@ -1,12 +1,20 @@
-'use client'
-import React, { useState } from 'react';
+"use client";
+
+import React, { useEffect, useState } from 'react';
+
+interface errTypes {
+  name?: string;
+  email?: string;
+  message?: string;
+}
 
 const PageComponent: React.FC = () => {
   const [name, setName] = useState<string>('');
   const [mail, setMail] = useState<string>('');
   const [message, setMessage] = useState<string>('');
+  const [error, setError] = useState<errTypes>({});
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     switch (name) {
       case 'name':
@@ -23,87 +31,95 @@ const PageComponent: React.FC = () => {
     }
   };
 
-  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+  useEffect(() => {
+    const errors: errTypes = {};
+    if (!name.trim()) {
+      errors.name = 'required!';
+    }
+    if (!mail.trim()) {
+      errors.email = 'required!';
+    } else if (!isValidEmail(mail)) {
+      errors.email = 'Invalid email format';
+    }
+    if (!message.trim()) {
+      errors.message = 'required!';
+    }
+    setError(errors); // Update the error state with the errors object
+  }, [name, mail, message]);
+
+  const isValidEmail = (mail: string): boolean => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(mail);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // Add form submission logic here if needed.
   };
 
   return (
     <div className='h-[80vh] flex items-center justify-center'>
-      <form
-        className='w-full max-w-sm p-6 rounded shadow-md'
-        onSubmit={handleSubmit}
-      >
-        <div className='mb-4 relative'>
+      <form className='w-full max-w-sm p-6 rounded shadow-md' onSubmit={handleSubmit}>
+        <div className='relative z-0 w-full mb-5'>
           <input
             type='text'
             name='name'
             required
             value={name}
-            className={`w-full p-2 rounded appearance-none focus:outline-none focus:ring-0 focus:border-black ${
-              name ? 'border-b-2' : ''
-            }`}
             onChange={handleChange}
+            placeholder=' '
+            className={`pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 ${error.name && 'dark:border-red-800 border-red-500'} appearance-none focus:outline-none transition duration-300 focus:ring-0 focus:border-black dark:focus:border-white  border-gray-500`}
           />
-          <label
-            htmlFor='name'
-            className={`absolute duration-300 top-2 left-2 transform origin-0 text-gray-500 ${
-              name ? 'text-black text-xs' : 'text-base'
-            } ${name ? '-translate-y-6' : 'translate-y-0'}`}
-          >
-            Name
-          </label>
-          <div className='text-red text-sm'>{name}</div>
+         
+            <label htmlFor='name' className='absolute duration-300 top-3 -z-1 origin-0 text-gray-500'>
+              Enter name
+            </label>
+          
         </div>
-        <div className='mb-4 relative'>
-          <input
-            type='email'
+        <div className='relative z-0 w-full mb-5'>
+        <input
+            type='text'
             name='mail'
             required
             value={mail}
-            className={`w-full p-2 rounded appearance-none focus:outline-none focus:ring-0 focus:border-black ${
-              mail ? 'border-b-2' : ''
-            }`}
             onChange={handleChange}
+            placeholder=' '
+            className={`pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 ${error.name && 'dark:border-red-800 border-red-500'} appearance-none focus:outline-none transition duration-300 focus:ring-0 focus:border-black dark:focus:border-white  border-gray-500`}
           />
           <label
             htmlFor='mail'
-            className={`absolute duration-300 top-2 left-2 transform origin-0 text-gray-500 ${
-              mail ? 'text-black text-xs' : 'text-base'
-            } ${mail ? '-translate-y-6' : 'translate-y-0'}`}
+            className='absolute duration-300 top-3 -z-1 origin-0 text-gray-500'
           >
-            Mail
+            Enter Mail
           </label>
-          <div className='text-red text-sm'>{mail}</div>
         </div>
-        <div className='mb-4 relative'>
-          <input
-            type='text'
-            required
+        <div className='relative z-0 w-full mb-5'>
+          <textarea
             name='message'
+            required
             value={message}
-            className={`w-full p-2 rounded appearance-none focus:outline-none focus:ring-0 focus:border-black ${
-              message ? 'border-b-2' : ''
-            }`}
             onChange={handleChange}
-          />
+            placeholder=' '
+            className={`pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 ${error.name && 'dark:border-red-800 border-red-500'} appearance-none focus:outline-none transition duration-300 focus:ring-0 focus:border-black dark:focus:border-white border-gray-500`}
+            />
           <label
             htmlFor='message'
-            className={`absolute duration-300 top-2 left-2 transform origin-0 text-gray-500 ${
-              message ? 'text-black text-xs' : 'text-base'
-            } ${message ? '-translate-y-6' : 'translate-y-0'}`}
+            className='absolute duration-300 top-3 -z-1 origin-0 text-gray-500'
           >
-            Message
+            Enter Message
           </label>
-          <div className='text-red text-sm'>{message}</div>
+      
         </div>
-        <div className='text-center'>
-          <button
-            type='submit'
-            className='px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600'
-          >
-            Submit
-          </button>
+        <div className=' flex w-0  group'>
+        <button
+  type='submit'
+  className='px-4 py-2 text-white bg-black rounded  hover:bg-[#323138] dark:bg-white dark:text-gray-700 dark:group-hover:bg-[#323138] dark:group-hover:text-white transition duration-300'
+>
+  Submit
+</button>
+
+
+
         </div>
       </form>
     </div>
