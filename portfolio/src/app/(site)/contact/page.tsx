cc2fb779ef44d39df948a31ba08b7d2da050296c
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-07-27 21:59:02 +0500

    update

--- portfolio/src/app/(site)/contact/page.tsx
+++ portfolio/src/app/(site)/contact/page.tsx
@@ -1,8 +1,9 @@
-"use client";
+'use client'
 
 import React, { useEffect, useState } from "react";
-import { toast } from "react-hot-toast";
-interface errTypes {
+import toast from "react-hot-toast";
+
+interface ErrTypes {
   name?: string;
   email?: string;
   message?: string;
@@ -10,9 +11,9 @@ interface errTypes {
 
 const PageComponent: React.FC = () => {
   const [name, setName] = useState<string>("");
-  const [mail, setMail] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
   const [message, setMessage] = useState<string>("");
-  const [error, setError] = useState<errTypes>({});
+  const [error, setError] = useState<ErrTypes>({});
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -22,8 +23,8 @@ const PageComponent: React.FC = () => {
       case "name":
         setName(value);
         break;
-      case "mail":
-        setMail(value);
+      case "email":
+        setEmail(value);
         break;
       case "message":
         setMessage(value);
@@ -33,60 +34,69 @@ const PageComponent: React.FC = () => {
     }
   };
 
+  const isValidEmail = (email: string): boolean => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  };
+
   useEffect(() => {
-    const errors: errTypes = {};
+    const errors: ErrTypes = {};
     if (!name.trim()) {
       errors.name = "required!";
     }
-    if (!mail.trim()) {
+    if (!email.trim()) {
       errors.email = "required!";
-    } else if (!isValidEmail(mail)) {
+    } else if (!isValidEmail(email)) {
       errors.email = "Invalid email format";
     }
     if (!message.trim()) {
       errors.message = "required!";
     }
-    setError(errors); // Update the error state with the errors object
-  }, [setName, mail, message]);
+    setError(errors);
+  }, [name, email, message]);
 
-  const isValidEmail = (mail: string): boolean => {
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailPattern.test(mail);
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Add form submission logic here if needed.
+    if (!isValidEmail(email) || !name.trim() || !message.trim()) {
+      toast.error("Please fill in all required fields correctly!");
+      return;
+    }
+
+    try {
+      await sender({ name, email, message });
+      setName("");
+      setEmail("");
+      setMessage("");
+      toast.success("Message sent Successfully!");
+    } catch (error) {
+      toast.error("Error sending Message!");
+      console.log("error sending message:", error);
+    }
   };
 
-  const sendMail = async (): Promise<void> => {
+  const sender = async (formData: {
+    name: string;
+    email: string;
+    message: string;
+  }) => {
     try {
-      const response = await fetch("/api/contactMail", {
-        method: "post",
+      const response = await fetch("/api", {
+        method:"POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          name: name,
-          email: mail,
-          message: message,
-        }),
+        body: JSON.stringify(formData),
       });
-  
-      if (response.ok) {
-        // Clear the form fields after successful email send
-        setName("");
-        setMail("");
-        setMessage("");
-      } else {
-        toast.error("Error sending email!");
+
+      if (!response.ok) {
+        throw new Error("Error sending email!");
       }
     } catch (error) {
       console.error("Error sending email:", error);
-      toast.error("Error sending email!");
+      throw error;
     }
   };
-  
+
   return (
     <div className="h-[80vh] flex items-center justify-center">
       <form
@@ -116,13 +126,13 @@ const PageComponent: React.FC = () => {
         <div className="relative z-0 w-full mb-5">
           <input
             type="text"
-            name="mail"
+            name="email"
             required
-            value={mail}
+            value={email}
             onChange={handleChange}
             placeholder=" "
             className={`pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 ${
-              error.name && "dark:border-red-800 border-red-500"
+              error.email && "dark:border-red-800 border-red-500"
             } appearance-none focus:outline-none transition duration-300 focus:ring-0 focus:border-black dark:focus:border-white  border-gray-500`}
           />
           <label
@@ -140,7 +150,7 @@ const PageComponent: React.FC = () => {
             onChange={handleChange}
             placeholder=" "
             className={`pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 ${
-              error.name && "dark:border-red-800 border-red-500"
+              error.message && "dark:border-red-800 border-red-500"
             } appearance-none focus:outline-none transition duration-300 focus:ring-0 focus:border-black dark:focus:border-white border-gray-500`}
           />
           <label
--- portfolio/src/app/api/route.ts
+++ portfolio/src/app/api/route.ts
@@ -1,47 +1,65 @@
-import nodemailer from 'nodemailer';
+import * as nodemailer from 'nodemailer';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const transporter =nodemailer.createTransport({
-  service:'gmail',
-  host:'smpt.gmail.com',
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
   secure: false,
-  auth:{},
-  });
-  function generatePakistanDate(): string {
-    const date = new Date();
-    const offset = date.getTimezoneOffset();
-    const pakistanOffset = 300; // Offset for Pakistan timezone in minutes (+5 hours * 60 minutes)
-  
-    // Adjust the date by adding the offset
-    date.setMinutes(date.getMinutes() + offset + pakistanOffset);
-  
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-  
-    return `${year}-${month}-${day}`;
-  }
-  
-  const date: string = generatePakistanDate();
-  
+  auth: {
+    user: process.env.NEXT_PUBLIC_GMAIL,
+    pass: process.env.NEXT_PUBLIC_GMAIL_PASS,
+  },
+});
 
-  export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== "POST") {
-      res.status(405).json({ message: "Method Not Allowed" });
-      return;
-    }
-    const {
-      email,
-      name,
-      address,
-      products,
-      zip,
-      city,
-      Id,
-      tlPrice,
-      tlQty,
-      phone,
-    } = req.body;
-    // Add your logic here based on the request body data
+interface Data {
+  message?: string;
+  name?: string;
+  email?: string;
+}
+
+export async function POST(req: NextApiRequest, res: NextApiResponse<Data>): Promise<void> {
+  if (req.method !== 'POST') {
+    res.status(405).json({ message: 'Method Not Allowed' });
+    return;
   }
-  
+
+  // Check if the required fields are present in the request body
+  const { name, email, message } = req.body;
+
+  try {
+    const mailOptions = {
+      from: 'Client Message',
+      to: 'contact.muhammadmubashir@gmail.com',
+      subject: 'New Message!',
+      html: `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>New Message from Contact Form</title>
+        </head>
+        <body style="font-family: Arial, sans-serif; line-height: 1.6; margin: 0; padding: 0; background-color: #f2f2f2;">
+          <div style="max-width: 600px; margin: 0 auto; padding: 20px;">
+            <h2 style="color: #333; text-align: center;">New Message from Contact Form!</h2>
+            <p style="font-size: 16px; color: #333;">You have received a new message from the contact form on your website. Please find the details below:</p>
+            <ul style="list-style: none; padding: 0;">
+              <li style="font-size: 16px; color: #333;">Name: ${name}</li>
+              <li style="font-size: 16px; color: #333;">Email: ${email}</li>
+              <li style="font-size: 16px; color: #333;">Message: ${message}</li>
+            </ul>
+          </div>
+        </body>
+        </html>
+      `,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res.status(200).json({ message: 'Email sent successfully' });
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).json({ message: 'Error sending email' });
+  }
+}
