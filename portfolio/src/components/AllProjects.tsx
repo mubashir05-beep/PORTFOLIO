5568c439345d84623a73764c684d8c39a029e6ed
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-08-01 18:48:36 +0500

    Update AllProjects.tsx

--- portfolio/src/components/AllProjects.tsx
+++ portfolio/src/components/AllProjects.tsx
@@ -1,13 +1,16 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import { categories } from '../../sanity/sanity-utils';
+'use client'
+import React, { useEffect, useState } from "react";
+import { categories } from "../../sanity/sanity-utils";
 
 interface Category {
-  tech_stack: string;
+  tech_stack: string[];
 }
 
 const AllProjects: React.FC = () => {
-  const [categoriesProjects, setCategoriesProjects] = useState<Category[] | null>(null);
+  const [categoriesProjects, setCategoriesProjects] = useState<
+    Category[] | null
+  >(null);
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -15,7 +18,7 @@ const AllProjects: React.FC = () => {
         const fetchedCategories: Category[] = await categories();
         setCategoriesProjects(fetchedCategories);
       } catch (error) {
-        console.error('Error fetching categories:', error);
+        console.error("Error fetching categories:", error);
         // Handle the error here (e.g., show an error message to the user).
       }
     };
@@ -23,18 +26,35 @@ const AllProjects: React.FC = () => {
     fetchCategories();
   }, []);
 
+  const handleSelection = (stack: string): void => {
+    setSelectedOption((prevSelectedOption) => {
+      // Use the callback form to log the updated value immediately
+      console.log(stack);
+      return stack;
+    });
+  };
+
   return (
-    <div className="flex gap-4 flex-wrap text-sm">
-    {categoriesProjects && categoriesProjects.map((category) => (
-      <div
-        key={category.tech_stack}
-        className="bg-gray-100 dark:bg-gray-800 px-2 py-1 rounded text-gray-800 dark:text-gray-200"
-      >
-        {category.tech_stack}
+    <div>
+      <div className="flex flex-wrap gap-2">
+        {categoriesProjects &&
+          categoriesProjects.map((tech) =>
+            tech.tech_stack.map((stack) => (
+              <div
+                key={stack}
+                className={`bg-gray-100 dark:bg-gray-800 cursor-pointer px-2 py-1 rounded ${
+                  selectedOption === stack
+                    ? "bg-[#323138] text-white dark:bg-gray-600"
+                    : "text-gray-800 dark:text-gray-200"
+                }`}
+                onClick={() => handleSelection(stack)}
+              >
+                {stack}
+              </div>
+            ))
+          )}
       </div>
-    ))}
-  </div>
-  
+    </div>
   );
 };
 
