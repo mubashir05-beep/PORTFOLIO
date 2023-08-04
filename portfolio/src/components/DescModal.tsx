4b87973f7ef32fb47a860dc47b29ee582fef0021
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-08-04 16:47:35 +0500

    update

--- portfolio/src/components/DescModal.tsx
+++ portfolio/src/components/DescModal.tsx
@@ -1,7 +1,10 @@
-'use client'
-import React, { useState, useEffect } from 'react';
+"use client";
+
+import Image from "next/image";
+import React, { useState, useEffect } from "react";
 import { BsArrowUpRightCircleFill } from "react-icons/bs";
 import { RxCross1 } from "react-icons/rx";
+import { urlFor } from "../../sanity/config/client-config";
 interface Projects {
   _id: string;
   thumbnail_image: {
@@ -12,8 +15,8 @@ interface Projects {
   project_name: string;
   category: string[];
   slug: string;
-  project_status: string,
-  project_industry: string,
+  project_status: string;
+  project_industry: string;
   project_description: string;
   project_caution: string;
   project_date: string;
@@ -27,12 +30,12 @@ const DescModal: React.FC<{ project: Projects }> = ({ project }) => {
 
   useEffect(() => {
     const handleBodyOverflow = () => {
-      document.body.style.overflow = modalOpen ? 'hidden' : 'unset';
+      document.body.style.overflow = modalOpen ? "hidden" : "unset";
     };
     handleBodyOverflow();
 
     return () => {
-      document.body.style.overflow = 'unset'; // Restore body overflow on component unmount
+      document.body.style.overflow = "unset"; // Restore body overflow on component unmount
     };
   }, [modalOpen]);
   const handleModalToggle = () => {
@@ -42,37 +45,86 @@ const DescModal: React.FC<{ project: Projects }> = ({ project }) => {
   return (
     <>
       {modalOpen && (
-      <div className="fixed inset-0 z-50 flex items-center justify-center ">
-
-        <div className="fixed inset-0 -z-10 bg-black opacity-60" onClick={handleModalToggle} />
-        <div className="dark:bg-[#323138] bg-white rounded-lg inset-0 max-[600px]:w-full max-[600px]:rounded-none max-[600px]:absolute overflow-auto shadow-lg min-[599px]:max-w-md p-6  flex  flex-col gap-[1rem]">
-            <div className='flex flex-col'>
-            <div className='flex  items-center justify-between'>
-              <h2 className="text-[2rem] dark:text-white  font-[500]">{project.project_name}</h2>
-              <RxCross1 size={24} className='cursor-pointer' onClick={handleModalToggle}/>
-            </div>
-              <h2 className="text-[14px] dark:text-gray-300">{project.project_status}</h2>
-              {project.project_caution &&
-                <div className='text-red-500 pt-[6px] text-xs'>{project.project_caution}</div>
-              }
-            </div>
-           
-            <div className='flex flex-col gap-[1rem]'>
-              <p className="dark:text-gray-300 text-sm">{project.project_description}</p>
-              <div className='flex flex-col gapz'>
-                <div className='dark:text-white text-[1rem] font-[500]'>Project Industry</div>
-                <div className='text-sm dark:text-gray-300'>{project.project_industry}</div>
+        <div className="fixed inset-0 z-50 flex items-center justify-center ">
+          <div
+            className="fixed inset-0 -z-10 bg-black opacity-60"
+            onClick={handleModalToggle}
+          />
+          <div className="dark:bg-[#323138] bg-white rounded-lg inset-0 max-[600px]:w-full  h-[664px] max-[600px]:h-full  w-[580px] max-[600px]:rounded-none max-[600px]:absolute overflow-auto shadow-lg min-[599px]:max-w-md p-6  flex  flex-col gap-[1rem]">
+            <div className="flex flex-col">
+              <div className="flex  items-center justify-between">
+                <h2 className="text-[2rem] dark:text-white  font-[500]">
+                  {project.project_name}
+                </h2>
+                <RxCross1
+                  size={24}
+                  className="cursor-pointer"
+                  onClick={handleModalToggle}
+                />
               </div>
-              <div className='flex flex-col gap-2'>
-                <div className='dark:text-white text-[1rem] font-[500]'>Tech Stack:</div>
+              <h2 className="text-[14px] dark:text-gray-300">
+                {project.project_status}
+              </h2>
+              {project.project_caution && (
+                <div className="text-red-500 pt-[6px] text-xs">
+                  {project.project_caution}
+                </div>
+              )}
+            </div>
+            {project.thumbnail_image && (
+              <Image
+                src={urlFor(project.thumbnail_image).url()}
+                alt={project.project_name}
+                width={800}
+                quality={100}
+                height={800}
+                className="object-contain rounded-lg border dark:border-white  w-full border-gray-600 flex-1"
+              />
+            )}
+            <div className="flex flex-col gap-[1rem]">
+              <p className="dark:text-gray-300 text-sm">
+                {project.project_description}
+              </p>
+              <div className="flex flex-col gapz">
+                <div className="dark:text-white text-[1rem] font-[500]">
+                  Project Industry
+                </div>
+                <div className="text-sm dark:text-gray-300">
+                  {project.project_industry}
+                </div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <div className="dark:text-white text-[1rem] font-[500]">
+                  Tech Stack:
+                </div>
                 <ul className="flex gap-1 flex-wrap">
                   {project.project_stack.map((tech, index) => (
-                    <li key={index} className={`bg-black dark:text-white text-gray-300 dark:bg-gray-800  px-2 text-sm py-1 rounded`}>{tech}</li>
+                    <li
+                      key={index}
+                      className={`bg-black dark:text-white text-gray-300 dark:bg-gray-800  px-2 text-sm py-1 rounded`}
+                    >
+                      {tech}
+                    </li>
                   ))}
-                </ul></div>
+                </ul>
+              </div>
               <div className="flex justify-between items-center pt-4">
-                <a href={project.live_link} target="_blank" rel="noopener noreferrer" className="dark:bg-white transition duration-300 text-white bg-black dark:text-black px-4 py-2 rounded-md hover:bg-gray-400">Live Demo</a>
-                <a href={project.repo_link} target="_blank" rel="noopener noreferrer"  className="dark:bg-white transition duration-300 text-white bg-black dark:text-black px-4 py-2 rounded-md hover:bg-gray-400">GitHub Repo</a>
+                <a
+                  href={project.live_link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="dark:bg-white transition duration-300 text-white bg-black dark:text-black px-4 py-2 rounded-md hover:bg-gray-400"
+                >
+                  Live Demo
+                </a>
+                <a
+                  href={project.repo_link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="dark:bg-white transition duration-300 text-white bg-black dark:text-black px-4 py-2 rounded-md hover:bg-gray-400"
+                >
+                  GitHub Repo
+                </a>
               </div>
             </div>
           </div>
--- portfolio/src/components/Project_Showcase.tsx
+++ portfolio/src/components/Project_Showcase.tsx
@@ -7,7 +7,6 @@ import { BsArrowUpRightCircleFill } from "react-icons/bs";
 import Link from "next/link";
 import DescModal from "./DescModal";
 interface projectsTypes {
- 
   _id: string;
   thumbnail_image: {
     asset: {
@@ -17,55 +16,56 @@ interface projectsTypes {
   project_name: string;
   category: string[];
   slug: string;
-  project_status:string,
-  project_industry:string,
+  project_status: string;
+  project_industry: string;
   project_description: string;
-  project_caution:string;
-  project_date:string;
+  project_caution: string;
+  project_date: string;
   project_stack: string[];
   live_link: string;
   repo_link: string;
-  
 }
 
 const Project_Showcase: React.FC = async () => {
   const projects: projectsTypes[] = await getProjects();
-
+  const truncateDescription = (description: string, numWords: number): string => {
+    const words = description.split(" ");
+    const truncatedWords = words.slice(0, numWords);
+    return truncatedWords.join(" ") + (words.length > numWords ? " ..." : "");
+  };
   return (
     <div className="p-4 border border-black flex flex-col gap-[2rem] dark:border-white rounded-lg">
       {projects.map((project: projectsTypes) => (
         <div
-          className="flex  max-[1201px]:flex-col-reverse border-b py-[1rem] dark:border-white justify-between gap-8"
+          className="flex items-center max-[1201px]:flex-col-reverse border-b py-[1rem] dark:border-white justify-between gap-8"
           key={project._id}
         >
           <div className="flex flex-col justify-between flex-1 gap-8">
             <div>
-            <div className="flex flex-col gap-2">
-              <div className="flex items-center max-[410px]:items-baseline max-[410px]:flex-col justify-between">
-                <div className="text-2xl font-semibold">
-                  {project.project_name}
-                </div>
-                <div className="text-[16px]">
-                  {project.project_status}
-                </div>
-              </div>
-              <div className="text-sm  text-gray-800 max-w-[800px] dark:text-gray-300">
-                {project.project_description}
-              </div>
-            </div>
-            <div className="flex gap-2 flex-col">
-              <p className="text-base font-semibold">Tech Stack :</p>
-              <span className="flex gap-1  flex-wrap text-sm">
-                {project.project_stack.map((tech: string) => (
-                  <div
-                    key={tech}
-                    className={`bg-black text-white dark:bg-gray-800  px-2 py-1 rounded`}
-                  >
-                    {tech}
+              <div className="flex flex-col gap-2">
+                <div className="flex items-center max-[410px]:items-baseline max-[410px]:flex-col justify-between">
+                  <div className="text-2xl font-semibold">
+                    {project.project_name}
                   </div>
-                ))}
-              </span>
-            </div>
+                  <div className="text-[16px]">{project.project_status}</div>
+                </div>
+                <div className="text-sm  text-gray-800 max-w-[800px] dark:text-gray-300">
+                  {truncateDescription(project.project_description,30)}
+                </div>
+              </div>
+              <div className="flex gap-2 flex-col">
+                <p className="text-base font-semibold">Tech Stack :</p>
+                <span className="flex gap-1  flex-wrap text-sm">
+                  {project.project_stack.map((tech: string) => (
+                    <div
+                      key={tech}
+                      className={`bg-black text-white dark:bg-gray-800  px-2 py-1 rounded`}
+                    >
+                      {tech}
+                    </div>
+                  ))}
+                </span>
+              </div>
             </div>
             <div className="flex items-center justify-between">
               <div className="flex gap-4 items-center">
@@ -84,20 +84,20 @@ const Project_Showcase: React.FC = async () => {
                   Repo
                 </a>
               </div>
-              
-              <DescModal project={project}/>
-              
+
+              <DescModal project={project} />
             </div>
           </div>
-          {project.thumbnail_image&&
-      <Image
-      src={urlFor(project.thumbnail_image).url()}
-      alt={project.project_name}
-      width={800}
-      height={800}
-      className="object-cover rounded-lg border dark:border-white min-[1201px]:max-w-[50%] max-w-[100%] w-full border-gray-600 flex-1"
-    />}
-    
+          {project.thumbnail_image && (
+            <Image
+              src={urlFor(project.thumbnail_image).url()}
+              alt={project.project_name}
+              width={800}
+              quality={100}
+              height={800}
+              className="object-contain h-full rounded-lg border dark:border-white min-[1201px]:max-w-[50%] max-w-[100%] w-full border-gray-600 flex-1"
+            />
+          )}
         </div>
       ))}
     </div>
